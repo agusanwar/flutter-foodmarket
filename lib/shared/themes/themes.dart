a600b2ada603286cec216ec71098a8fcffafdbb2
agusanwar <agusanwar@gmail.com>
2022-12-15 13:13:28 +0700

    setup for thames, assets, font poppins, color

---
+++ lib/shared/themes/themes.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+Color whiteColor = const Color(0xffFFFFFF);
+Color blackColor = const Color(0xff14193F);
+Color grayColor = const Color(0xffA4A8AE);
+Color gray2Color = const Color(0xff696B76);
+Color graySoftColor = const Color(0xffF1F1F9);
+Color backgraundLightColor = const Color(0xffF6F8FB);
+Color backgraundDarkColor = const Color(0xff020518);
+Color purpleColor = const Color(0xff695BE9);
+Color blueColor = const Color(0xff53C1F9);
+Color blueAccerColor = const Color(0xff007FFF);
+Color blueDarkColor = const Color(0xff0034C4);
+Color numberBackraundColor = const Color(0xff1A1D2E);
+Color redColor = const Color(0xffFF2566);
+Color greenColor = const Color(0xff22B07D);
+Color yellowColor = const Color(0xffFFDF34);
+Color orangeColor = const Color(0xffFFA234);
+
+TextStyle blackTextStyle = GoogleFonts.poppins(
+  color: blackColor,
+);
+TextStyle grayTextStyle = GoogleFonts.poppins(
+  color: grayColor,
+);
+TextStyle graydarkTextStyle = GoogleFonts.poppins(
+  color: gray2Color,
+);
+TextStyle whiteTextStyle = GoogleFonts.poppins(
+  color: whiteColor,
+);
+TextStyle blueTextStyle = GoogleFonts.poppins(
+  color: blueColor,
+);
+TextStyle greenTextStyle = GoogleFonts.poppins(
+  color: greenColor,
+);
+TextStyle purpleTextStyle = GoogleFonts.poppins(
+  color: purpleColor,
+);
+
+FontWeight light = FontWeight.w300;
+FontWeight regular = FontWeight.w400;
+FontWeight medium = FontWeight.w500;
+FontWeight semiBold = FontWeight.w600;
+FontWeight bold = FontWeight.w700;
+FontWeight extraBold = FontWeight.w800;
+FontWeight black = FontWeight.w900;
